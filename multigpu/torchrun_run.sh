a4a3ef5febe70ee06dc61bac650a14f2f0bf429e
h-takoyaki <h-somehow@qq.com>
2022-10-04 16:11:10 +0800

    multigpu: CUDA_VISIBLE_DEVICES

--- multigpu/torchrun_run.sh
+++ multigpu/torchrun_run.sh
@@ -1,5 +1,5 @@
 OMP_NUM_THREADS=12               \
-CUDA_VISIBLE_DEVICE=0,1,2,3,4,5,6,7     \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7     \
 torchrun     --standalone       \
              --nnodes=1         \
              --nproc_per_node=8 \
